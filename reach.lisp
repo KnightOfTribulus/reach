de7251f6d6b0eee247352318d89473cf699d0a5e
KnightOfTribulus <treflcrew@gmail.com>
2020-05-04 20:59:16 +0300

    second-simplified

--- reach.lisp
+++ reach.lisp
@@ -3,102 +3,26 @@
 (in-package #:reach)
 
 ;;(defparameter *net* (make-petri-net))
-(defclass location ()
-  ((%name :initarg :name
-	  :accessor name)
-   (%token-count :initarg :token-count
-		 :initform 0
-		 :accessor token-count)))
 
-(defun make-location (name count)
-  (make-instance 'location
-		 :token-count count
-		 :name name))
+;; the transition matrix, rows correspond to transitions, coluns correspond to positions
+;;                                   p1 p2
+(defparameter *transition-matrix* '((1  2)  ;t1
+				    (3  4)) ;t2
+  )
 
-(defmacro deflocation (name &optional count) ;; deprecated!!!
-  `(defparameter ,name (make-instance 'location
-				      ,@(if count
-					    `(:token-count ,count)
-					    '(:token-count 0)))))
+(defun transition-vector (i)
+  (elt *transition-matrix* i))
 
-(defclass in-bit ()
-  ((%from :initarg :from ;; location
-	  :accessor from)
-   (%takes :initarg :takes ;; number
-	   :initform 1
-	   :accessor takes)))
+(defun allowable-p (marking index)
+  (when (every #'identity
+	       (mapcar #'(lambda (m ti)
+			   (>= m ti))
+		       marking
+		       (transition-vector index)))))
 
-(defun make-in-bit (from takes)
-  (make-instance 'in-bit
-		 :from from
-		 :takes takes))
-
-(defun make-in-bits (pairs)
-  (mapcar
-   #'(lambda (pair)
-       (make-in-bit (car pair) (cadr pair)))
-   pairs))
-
-(defclass out-bit ()
-  ((%to :initarg :to ;; location
-	:accessor to)
-  (%gives :initarg :gives ;; number
-	  :initform 1
-	  :accessor gives)))
-
-(defun make-out-bit (to gives)
-  (make-instance 'out-bit
-		 :to to
-		 :gives gives))
-
-(defun make-out-bits (pairs)
-  (mapcar
-   #'(lambda (pair)
-       (make-out-bit (car pair) (cadr pair)))
-   pairs))
-
-(defclass transition ()
-  ((%name :initarg :name
-	  :accessor name)
-   (%in-bits :initarg :in-bits
-	     :accessor in-bits)
-   (%out-bits :initarg :out-bits
-	      :accessor out-bits)))
-
-(defun make-transition (name in-pairs out-pairs)
-  (make-instance 'transition
-		 :name name
-		 :in-bits (make-in-bits in-pairs) 
-		 :out-bits (make-out-bits out-pairs)))
-
-(defmethod allowed-p ((this transition))
-  (when (every #'(lambda (bit)
-		   (>= (token-count (from bit))
-		       (takes this)))
-	       (in-bits this))
-    t))
-
-(defmethod fire ((this transition))
-  (when (allowed-p this)
-    (with-accessors ((in-bits in-bits)
-		     (out-bits out-bits))
-	this
-      (mapcar
-       #'(lambda (inbit)
-	   (decf (token-count (from inbit)) (takes inbit)))
-       in-bits)
-      (mapcar
-       #'(lambda (outbit)
-	   (incf (token-count (to outbit)) (gives outbit)))
-       out-bits))))
-
-(defclass net ()
-  ((locations :initarg :locations
-	      :accessor locations)
-   (transitions :initarg :transitions
-		:accessor transitions)))
-
-(defun make-net (locations transitions)
-  ())
-
-
+(defun fire-transition (marking index)
+  (when (allowable-p marking index)
+    (mapcar #'(lambda (m ti)
+		 (+ m ti))
+	     marking
+	     (transition-vector index))))
